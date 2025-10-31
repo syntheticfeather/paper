a3d64883e3740a22fc00934b4c04273994cd3a9f
gcc <2391317090@qq.com>
2025-10-31 19:52:22 +0800

    编译技术-实验1-代码及笔记

---
+++ compile/practice/task1/test.c
@@ -0,0 +1,22 @@
+int main()
+{
+    return 3;
+}
+
+// 输入
+
+int main() { return 3; }
+
+// 预期结果
+
+/*
+<261, KEYWORD>
+<260, main>
+<285, SYMBOL>
+<286, SYMBOL>
+<287, SYMBOL>
+<269, KEYWORD>
+<258, 3>
+<292, SYMBOL>
+<288, SYMBOL>
+*/
